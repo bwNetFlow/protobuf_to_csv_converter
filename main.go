0f038e50102d084b3712afcadb9328f0b42c6c85
leobrada <leo.bradatsch@uni-ulm.de>
2019-08-13 14:56:53 +0000

    update

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
-	"reader"
 	"reflect"
 	"strconv"
 	"strings"
 	"time"
+    "net"
 
 	"github.com/Shopify/sarama"
 	kafka "github.com/bwNetFlow/kafkaconnector"
@@ -18,6 +19,27 @@ var kafkaConn = kafka.Connector{}
 var usrcrd = flag.String("u", "", "Path for user credential file")
 var path = flag.String("p", "", "Path to CSV file that is created")
 var duration = flag.Int("d", 5, "Amount of time that is written into one file")
+
+func readIni(path string) (content map[string]string, ok bool) {
+	fmt.Fprintf(os.Stdout, "Trying to read from file %s\n", path)
+	ini, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ioutil.ReadFile: Error reading %s (%v)\n", path, err)
+		ok = false
+		return content, ok
+	}
+	content = make(map[string]string)
+	var tmp []string
+	for _, line := range strings.Split(string(ini), "\n") {
+		if len(line) == 0 {
+			continue
+		}
+		tmp = strings.Split(line, "=")
+		content[strings.Trim(tmp[0], " ")] = strings.Trim(tmp[1], " ")
+	}
+	ok = true
+	return content, ok
+}
 
 func expandDatePart(part string) string {
 	var expandedPart string
@@ -47,11 +69,11 @@ func timeout(mins time.Duration, ch chan<- bool) {
 	ch <- true
 }
 
-func connectToKafka() {
-	broker := "kafka01.bwnf.belwue.de:9093, kafka02.bwnf.belwue.de:9093, kafka03.bwnf.belwue.de:9093, kafka04.bwnf.belwue.de:9093, kafka05.bwnf.belwue.de:9093"
-	topic := []string{"flow-messages-anon"}
-	consumerGroup := "anon-golang-example"
-	kafkaConn.SetAuthAnon()
+func connectToKafka(credentials map[string]string) {
+    broker := credentials["brokers"]
+    topic := []string{credentials["topic"]}
+    consumerGroup := credentials["grp_id"]
+    kafkaConn.SetAuth(credentials["user"], credentials["pwd"])
 	kafkaConn.StartConsumer(broker, topic, consumerGroup, sarama.OffsetNewest)
 }
 
@@ -74,9 +96,15 @@ func writeToCsv(file *os.File, fields []string) {
 		return
 	}
 	reflected_flow := reflect.ValueOf(flow)
+    var addr net.IP
 	for _, fieldname := range fields {
 		field := reflect.Indirect(reflected_flow).FieldByName(fieldname)
-		csv_line = csv_line + fmt.Sprint(field) + ","
+        if field.Kind() == reflect.Slice && reflect.ValueOf(field.Bytes()[0]).Kind() == reflect.Uint8 {
+            addr = net.IP(field.Bytes())
+            csv_line = csv_line + fmt.Sprint(addr) + ","
+        } else {
+		    csv_line = csv_line + fmt.Sprint(field) + ","
+        }
 	}
 	csv_line_len = len(csv_line)
 	csv_line = csv_line[:csv_line_len-1]
@@ -87,12 +115,9 @@ func main() {
 	flag.Parse()
 	fields := flag.Args()
 
-	credentials, _ := reader.readIni(*usrcrd)
-	for k, v := range credentials {
-		fmt.Fprintf("%s:%s\n", k, v)
-	}
+	credentials, _ := readIni(*usrcrd)
 
-	connectToKafka()
+	connectToKafka(credentials)
 	defer kafkaConn.Close()
 
 	start := time.Now()
